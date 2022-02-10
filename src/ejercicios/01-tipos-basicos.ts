6774eef7eb19b9f8d3b8b90e18736ad961a25345
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-02-10 18:14:17 -0500

    Se añade proyecto inicial

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,22 @@
+/*
+    ===== TIPOS DE DATOS =====
+*/
+
+
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// Boolean
+let estaVivo: boolean = true;
+
+// String
+let nombre: string = "Alejandra";
+
+// Varios Tipos
+let hp: number | string = 95;
+
+// NOTAS: Todo lo que hagamos en Typescript, se traduce a su equivalente en javascript cuando lo pasemos al navegador 
+// (Debido a que este entiende js no ts)
