be83d5099d91ce88f3b5311e97e1e46d78ea4244
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-02-11 12:17:40 -0500

    Se añade ejercicio de desestructuracion de funcion

---
+++ src/ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,39 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+/*
+    ===== DESESTRUCTURACION FUNCION =====
+*/
+
+interface Producto {
+    Descripcion: string;
+    Precio: number;
+}
+
+const telefono: Producto = {
+    Descripcion: "Samsung A21",
+    Precio: 200
+}
+
+const iphone: Producto = {
+    Descripcion: "Iphone X",
+    Precio: 1000
+}
+
+function CalcularISV(productos: Producto[]): [number, number] {
+    let total = 0;
+    productos.forEach(({ Precio }) => {
+        total += Precio;
+    });
+
+    return [total, total * 0.15];
+}
+
+const productos = [ telefono, iphone ];
+const [total, isv] = CalcularISV(productos);
+
+console.log("Total: ", total);
+console.log("ISV: ", isv);
+
+// ===== NOTAS ===== 
